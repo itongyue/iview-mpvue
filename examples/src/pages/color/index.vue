7a8ae514f2aff3d33e98897bcd5282e3bafdc524
Peak <fujizhong@163.com>
2020-07-02 18:14:45 +0800

    mpvue 会把命名的 slot 所在的 div 丢弃

--- examples/src/pages/color/index.vue
+++ examples/src/pages/color/index.vue
@@ -4,25 +4,31 @@
     <i-row>
       <i-col i-class="color-col" span="12">
         <i-card i-class="color">
-          <div slot="content" class="color-item primary">
-            <div class="color-item-title">Primary</div>
-            <div class="color-item-content">#2d8cf0</div>
+          <div slot="content">
+            <div class="color-item primary">
+              <div class="color-item-title">Primary</div>
+              <div class="color-item-content">#2d8cf0</div>
+            </div>
           </div>
         </i-card>
       </i-col>
       <i-col i-class="color-col" span="12">
         <i-card i-class="color">
-          <div slot="content" class="color-item light-primary">
-            <div class="color-item-title">Light Primary</div>
-            <div class="color-item-content">#5cadff</div>
+          <div slot="content">
+            <div class="color-item light-primary">
+              <div class="color-item-title">Light Primary</div>
+              <div class="color-item-content">#5cadff</div>
+            </div>
           </div>
         </i-card>
       </i-col>
       <i-col i-class="color-col" span="12">
         <i-card i-class="color">
-          <div slot="content" class="color-item dark-primary">
-            <div class="color-item-title">Dark Primary</div>
-            <div class="color-item-content">#2b85e4</div>
+          <div slot="content">
+            <div class="color-item dark-primary">
+              <div class="color-item-title">Dark Primary</div>
+              <div class="color-item-content">#2b85e4</div>
+            </div>
           </div>
         </i-card>
       </i-col>
@@ -31,25 +37,31 @@
     <i-row>
       <i-col i-class="color-col" span="12">
         <i-card i-class="color">
-          <div slot="content" class="color-item success">
-            <div class="color-item-title">Success</div>
-            <div class="color-item-content">#19be6b</div>
+          <div slot="content">
+            <div class="color-item success">
+              <div class="color-item-title">Success</div>
+              <div class="color-item-content">#19be6b</div>
+            </div>
           </div>
         </i-card>
       </i-col>
       <i-col i-class="color-col" span="12">
         <i-card i-class="color">
-          <div slot="content" class="color-item warning">
-            <div class="color-item-title">Warning</div>
-            <div class="color-item-content">#ff9900</div>
+          <div slot="content">
+            <div class="color-item warning">
+              <div class="color-item-title">Warning</div>
+              <div class="color-item-content">#ff9900</div>
+            </div>
           </div>
         </i-card>
       </i-col>
       <i-col i-class="color-col" span="12">
         <i-card i-class="color">
-          <div slot="content" class="color-item error">
-            <div class="color-item-title">Error</div>
-            <div class="color-item-content">#ed3f14</div>
+          <div slot="content">
+            <div class="color-item error">
+              <div class="color-item-title">Error</div>
+              <div class="color-item-content">#ed3f14</div>
+            </div>
           </div>
         </i-card>
       </i-col>
@@ -58,57 +70,71 @@
     <i-row>
       <i-col i-class="color-col" span="12">
         <i-card i-class="color">
-          <div slot="content" class="color-item title">
-            <div class="color-item-title">标题 Title</div>
-            <div class="color-item-content">#1c2438</div>
+          <div slot="content">
+            <div class="color-item title">
+              <div class="color-item-title">标题 Title</div>
+              <div class="color-item-content">#1c2438</div>
+            </div>
           </div>
         </i-card>
       </i-col>
       <i-col i-class="color-col" span="12">
         <i-card i-class="color">
-          <div slot="content" class="color-item content">
-            <div class="color-item-title">正文 Content</div>
-            <div class="color-item-content">#495060</div>
+          <div slot="content">
+            <div class="color-item content">
+              <div class="color-item-title">正文 Content</div>
+              <div class="color-item-content">#495060</div>
+            </div>
           </div>
         </i-card>
       </i-col>
       <i-col i-class="color-col" span="12">
         <i-card i-class="color">
-          <div slot="content" class="color-item sub-color">
-            <div class="color-item-title">辅助 Sub Color</div>
-            <div class="color-item-content">#ed3f14</div>
+          <div slot="content">
+            <div class="color-item sub-color">
+              <div class="color-item-title">辅助 Sub Color</div>
+              <div class="color-item-content">#ed3f14</div>
+            </div>
           </div>
         </i-card>
       </i-col>
       <i-col i-class="color-col" span="12">
         <i-card i-class="color">
-          <div slot="content" class="color-item disabled">
-            <div class="color-item-title">失效 Disabled</div>
-            <div class="color-item-content">#bbbec4</div>
+          <div slot="content">
+            <div class="color-item disabled">
+              <div class="color-item-title">失效 Disabled</div>
+              <div class="color-item-content">#bbbec4</div>
+            </div>
           </div>
         </i-card>
       </i-col>
       <i-col i-class="color-col" span="12">
         <i-card i-class="color">
-          <div slot="content" class="color-item border dark">
-            <div class="color-item-title">边框 Border</div>
-            <div class="color-item-content">#dddee1</div>
+          <div slot="content">
+            <div class="color-item border dark">
+              <div class="color-item-title">边框 Border</div>
+              <div class="color-item-content">#dddee1</div>
+            </div>
           </div>
         </i-card>
       </i-col>
       <i-col i-class="color-col" span="12">
         <i-card i-class="color">
-          <div slot="content" class="color-item divider dark">
-            <div class="color-item-title">分割线 Divider</div>
-            <div class="color-item-content">#e9eaec</div>
+          <div slot="content">
+            <div class="color-item divider dark">
+              <div class="color-item-title">分割线 Divider</div>
+              <div class="color-item-content">#e9eaec</div>
+            </div>
           </div>
         </i-card>
       </i-col>
       <i-col i-class="color-col" span="12">
         <i-card i-class="color">
-          <div slot="content" class="color-item background dark">
-            <div class="color-item-title">背景 Background</div>
-            <div class="color-item-content">#f8f8f9</div>
+          <div slot="content">
+            <div class="color-item background dark">
+              <div class="color-item-title">背景 Background</div>
+              <div class="color-item-content">#f8f8f9</div>
+            </div>
           </div>
         </i-card>
       </i-col>
@@ -116,84 +142,102 @@
   </div>
 </template>
 <script>
-import panel from '../../components/panel/panel';
-import row from '../../components/row/row';
-import col from '../../components/col/col';
-import card from '../../components/card/card';
-export default {
-  components: {
-    'i-panel': panel,
-    'i-row': row,
-    'i-col': col,
-    'i-card': card
-  }
-};
+  import panel from '../../components/panel/panel';
+  import row from '../../components/row/row';
+  import col from '../../components/col/col';
+  import card from '../../components/card/card';
+
+  export default {
+    components: {
+      'i-panel': panel,
+      'i-row': row,
+      'i-col': col,
+      'i-card': card
+    }
+  };
 </script>
 <style>
-.color {
+  .color {
     padding: 0 !important;
     border: none !important;
-}
-.color-col{
+  }
+
+  .color-col {
     margin-bottom: 16px;
-}
-.color-item{
+  }
+
+  .color-item {
     width: 100%;
     color: #fff;
     box-shadow: inset 0 0 0 1px rgba(0, 0, 0, 0.05);
     border-radius: 5px;
-}
-.color-item.dark{
+  }
+
+  .color-item.dark {
     color: #657180;
-}
-.color-item-title{
+  }
+
+  .color-item-title {
     padding: 6px 6px 0;
     font-size: 14px;
-}
-.color-item-content{
+  }
+
+  .color-item-content {
     padding: 6px;
     font-size: 12px;
     opacity: 0.7;
-}
+  }
 
-.color-item.primary{
+  .color-item.primary {
     background: #2d8cf0;
-}
-.color-item.light-primary{
+  }
+
+  .color-item.light-primary {
     background: #5cadff;
-}
-.color-item.dark-primary{
+  }
+
+  .color-item.dark-primary {
     background: #2b85e4;
-}
-.color-item.success{
+  }
+
+  .color-item.success {
     background: #19be6b;
-}
-.color-item.warning{
+  }
+
+  .color-item.warning {
     background: #ff9900;
-}
-.color-item.error{
+  }
+
+  .color-item.error {
     background: #ed3f14;
-}
-.color-item.title{
+  }
+
+  .color-item.title {
     background: #1c2438;
-}
-.color-item.content{
+  }
+
+  .color-item.content {
     background: #495060;
-}
-.color-item.sub-color{
+  }
+
+  .color-item.sub-color {
     background: #80848f;
-}
-.color-item.disabled{
+  }
+
+  .color-item.disabled {
     background: #bbbec4;
-}
-.color-item.border{
+  }
+
+  .color-item.border {
     background: #dddee1;
-}
-.color-item.divider{
+  }
+
+  .color-item.divider {
     background: #e9eaec;
-}
-.color-item.background{
+  }
+
+  .color-item.background {
     background: #f8f8f9;
-}
+  }
 </style>
 
