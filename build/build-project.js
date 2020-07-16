0f8788cbcb23829212a24ac4e2a83d7437a6a304
Peak <fujizhong@163.com>
2020-07-16 17:19:59 +0800

    增加 npm run project

---
+++ build/build-project.js
@@ -0,0 +1,95 @@
+
+const { getProjectPath, cssInjection } = require('./util/projectHelper')
+const transformLess = require('./util/transformLess')
+
+const path = require('path')
+const through2 = require('through2')
+const gulp = require('gulp')
+const rename = require('gulp-rename')
+const destDir = getProjectPath('dist')
+const babel = require("gulp-babel")
+const projectDestDir = getProjectPath('../src/components/iview')
+
+gulp.task('compile-css', done => {
+  return gulp
+    .src(['../src/**/*.less'])
+    .pipe(
+      through2.obj(function (file, encoding, next) {
+        this.push(file.clone())
+        if (file.path.match(/(\/|\\)style(\/|\\)(.*)\.less$/)
+          || file.path.match(/(\/|\\)style\.less$/)
+        ) {
+          transformLess(file.path)
+            .then(css => {
+              file.contents = Buffer.from(css)
+              file.path = file.path.replace(/\.less$/, '.css')
+              this.push(file)
+              next()
+            })
+            .catch(e => {
+              console.error(e)
+            })
+        }
+        else {
+          next()
+        }
+      })
+    )
+    .pipe(gulp.dest(destDir))
+    .pipe(gulp.dest(projectDestDir))
+    .pipe(rename({basename: 'iview'}))
+    .pipe(gulp.dest(path.join(projectDestDir, '../../../static/css')))
+})
+
+gulp.task('compile-js', () => {
+  return gulp
+    .src(['../src/**/*.js'])
+    .pipe(babel({
+      "plugins": ["@babel/plugin-proposal-object-rest-spread"],
+      "presets": [
+        "@babel/preset-env"
+      ]
+    }))
+    .pipe(
+      through2.obj(function (file, encoding, next) {
+        this.push(file.clone())
+        if (
+          file.path.match(/(\/|\\)style(\/|\\)index\.js/)
+        ) {
+          const content = file.contents.toString(encoding)
+          file.contents = Buffer.from(cssInjection(content))
+          file.path = file.path.replace(/index\.js/, 'css.js')
+          this.push(file)
+          next()
+        }
+        else {
+          next()
+        }
+      })
+    )
+    .pipe(gulp.dest(destDir))
+    .pipe(gulp.dest(projectDestDir))
+})
+
+gulp.task('copy-static', () => {
+  return gulp
+    .src(['../assets/**/*.@(png|svg|jpg)'])
+    .pipe(gulp.dest(path.join(destDir, 'assets')))
+    .pipe(gulp.dest(path.join(projectDestDir, 'assets')))
+})
+
+gulp.task('copy-vue', () => {
+  return gulp
+    .src(['../src/components/**/*.vue'])
+    .pipe(gulp.dest(path.join(destDir, 'components')))
+    .pipe(gulp.dest(path.join(projectDestDir, 'components')))
+})
+
+gulp.task('auto', () => {
+  gulp.watch('../src/**/*.js', gulp.series('compile-js'))
+  gulp.watch('../assets/**/*.@(png|svg|jpg)', gulp.series('copy-static'))
+  gulp.watch('../src/components/**/*.vue', gulp.series('copy-vue'))
+  gulp.watch('../src/**/*.less', gulp.series('compile-css'))
+})
+
+gulp.task('default', gulp.series('compile-css', 'compile-js', 'copy-vue','copy-static', 'auto'))
--- examples/src/pages/card/index.vue
+++ examples/src/pages/card/index.vue
@@ -7,12 +7,12 @@
     </i-card>
     <div style="margin: 16px">通栏</div>
     <i-card full="true" title="卡片标题" extra="额外内容" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
-      <p>slot="content">内容不错</p>
+      <p slot="content">内容不错</p>
       <p slot="footer">尾部内容</p>
     </i-card>
     <div style="margin: 16px">自定义图片样式</div>
     <i-card full="true" title="卡片标题" extra="额外内容" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg" i-class="card-demo">
-      <p>slot="content">内容不错</p>
+      <p slot="content">内容不错</p>
       <p slot="footer">尾部内容</p>
     </i-card>
   </div>
